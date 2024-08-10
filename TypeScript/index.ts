3681100cd7f1f27c71b1fbc278e1f0b23e6db6c9
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2024-08-10 23:57:36 +0900

    TypeScript側は完成した！

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -19,6 +19,84 @@ const optionSchema = object({
     title: optional(string())
 })
 
+
+async function NewActivity(title: string,NowDate: string) {
+    const start = await client.pages.create({
+        parent: {
+            database_id: process.env.DATABASE_ID!,
+            type: "database_id"
+        },
+        properties: {
+            "名前": {
+                type: "title",
+                title: [
+                    {
+                        text: {
+                            content: title
+                        }
+                    }
+                ]
+            },
+            "開始時刻": {
+                type: "date",
+                date: {
+                    start: NowDate,
+                    time_zone:  "Asia/Tokyo"
+                }
+            }
+        }
+    })
+    if(start.id){
+        console.log(`Success! new Activity ${NowDate}`)
+    }else {
+        console.error("Add Error")
+    }
+}
+
+async function EndActivity(NowDate: string) {
+    const history = await client.databases.query({
+        database_id: process.env.DATABASE_ID!,
+        filter: {
+            and: [
+                {
+                    property: "開始時刻",
+                    type: "date",
+                    date: {
+                        equals: formatToTimeZone(new Date(),"YYYY-MM-DD",{timeZone: "Asia/Tokyo"})
+                    }
+                    
+                }
+            ]
+        }
+    })
+    if(history.results.length !== 0){
+        const item  = history.results.shift()
+        if(item === undefined){
+            throw new Error("今日の活動は開始されていません")
+        }else {
+            const end = await client.pages.update({
+                page_id: item.id,
+                properties: {
+                    "終了時刻": {
+                        type: "date",
+                        date: {
+                            start: NowDate,
+                            time_zone: "Asia/Tokyo"
+                        }
+                    }
+                }
+            })
+            if(end.id){
+                console.log(`Success! new Activity ${NowDate}`)
+            }else {
+                console.error("Add Error")
+            }
+            console.log(`Congrats! Activity Ended ${NowDate}`)
+        }
+    }else {
+        console.error("今日の活動は開始されていません")
+    }
+}
   
 const commandline = new Command("Notion-Recorder")
 
@@ -34,106 +112,39 @@ commandline
         const NowDate = formatToTimeZone(new Date(),"YYYY-MM-DD HH:mm:ss",{timeZone: "Asia/Tokyo"})
         // console.log(NowDate)
         const {command,title} = parsedOption
-        if(command !== "times") {
+        if(command !== "times" && command !== "check") {
             if(title === undefined){
                 throw new Error("引数が足りません")
             }else {
                 console.log(NowDate + ": " + title)
                 if(command === "start"){
-                    const start = await client.pages.create({
-                        parent: {
-                            database_id: process.env.DATABASE_ID!,
-                            type: "database_id"
-                        },
-                        properties: {
-                            "名前": {
-                                type: "title",
-                                title: [
-                                    {
-                                        text: {
-                                            content: title
-                                        }
-                                    }
-                                ]
-                            },
-                            "開始時刻": {
-                                type: "date",
-                                date: {
-                                    start: NowDate,
-                                    time_zone:  "Asia/Tokyo"
-                                }
-                            }
-                        }
-                    })
-                    if(start.id){
-                        console.log(`Success! new Activity ${NowDate}`)
-                    }else {
-                        console.error("Add Error")
-                    }
+                    NewActivity(title,NowDate)
                 }
                 else if(command === "end") {
-                    const history = await client.databases.query({
-                        database_id: process.env.DATABASE_ID!,
-                        filter: {
-                            and: [
-                                {
-                                    property: "開始時刻",
-                                    type: "date",
-                                    date: {
-                                        equals: formatToTimeZone(new Date(),"YYYY-MM-DD",{timeZone: "Asia/Tokyo"})
-                                    }
-                                    
-                                }
-                            ]
-                        }
-                    })
-                    if(history.results.length !== 0){
-                        const item  = history.results.shift()
-                        if(item === undefined){
-                            throw new Error("今日の活動は開始されていません")
-                        }else {
-                            const end = await client.pages.update({
-                                page_id: item.id,
-                                properties: {
-                                    "終了時刻": {
-                                        type: "date",
-                                        date: {
-                                            start: NowDate,
-                                            time_zone: "Asia/Tokyo"
-                                        }
-                                    }
-                                }
-                            })
-                            if(end.id){
-                                console.log(`Success! new Activity ${NowDate}`)
-                            }else {
-                                console.error("Add Error")
-                            }
-                            console.log(`Congrats! Activity Ended ${NowDate}`)
-                        }
-                    }else {
-                        console.error("今日の活動は開始されていません")
-                    }
-                }else {
-                    const item = await client.databases.query({
-                        database_id: process.env.DATABASE_ID!,
-                        filter: {
-                            and: [
-                                {
-                                    property: "開始時刻",
-                                    type: "date",
-                                    date: {
-                                        equals: formatToTimeZone(new Date(),"YYYY-MM-DD",{timeZone: "Asia/Tokyo"})
-                                    }
-                                }
-                            ]
-                        }
-                    })
-                    console.log(item.results.length === 0 ? "まだ今日は活動できていません" : "今日は活動の記録があります")
+                    EndActivity(NowDate)
                 }
             }
         }else {
-            console.log("まだ実装されてません！")
+            if(command === "check"){
+                const item = await client.databases.query({
+                    database_id: process.env.DATABASE_ID!,
+                    filter: {
+                        and: [
+                            {
+                                property: "開始時刻",
+                                type: "date",
+                                date: {
+                                    equals: formatToTimeZone(new Date(),"YYYY-MM-DD",{timeZone: "Asia/Tokyo"})
+                                }
+                            }
+                        ]
+                    }
+                })
+                console.log(item.results.length === 0 ? "まだ今日は活動できていません" : "今日は活動の記録があります")
+            }else {
+                console.log(command,"は、まだ実装されてません！")
+            }
+           
         }
     })
 commandline.parse()
