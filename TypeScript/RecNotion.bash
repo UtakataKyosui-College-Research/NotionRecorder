a4a6e7b050da6c7f4758b61ab4511f94334b10a9
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2024-08-11 02:10:34 +0900

    もうこれで終わりとする

--- TypeScript/RecNotion.bash
+++ TypeScript/RecNotion.bash
@@ -1,1 +1,5 @@
-bun run index.ts
+# bun run index.ts
+# bun build index.ts --outdir .
+# bun nexe -i ファイル名.js -o ファイル名.exe -t windows-x64-14.15.3 -r .env
+bun nexe -i index.js -o RecNote.exe -t linux-x64-14.15.3 --resource ./.env
+ 
